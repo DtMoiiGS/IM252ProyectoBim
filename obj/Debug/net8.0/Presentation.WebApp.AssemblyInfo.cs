fac61056d4a5a26583fd843c5d4681f944b037ed
Cristofer Moises Garcia Sandoval <CristoferMoises@MacBook-Air-de-Cristofer-2.local>
2025-03-06 00:37:53 -0600

    Actualización del proyecto con mejoras en XYZ

--- obj/Debug/net8.0/Presentation.WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Presentation.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6bd37abf0ca0873c755836f8d1a92ada1b2c692")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
